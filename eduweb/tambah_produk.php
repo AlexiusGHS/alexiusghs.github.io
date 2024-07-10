d23bab9cb7f590471d5ce2112a60707d7e609cfa
AlexiusGHS <149881049+AlexiusGHS@users.noreply.github.com>
2024-07-10 22:18:47 +0700

    Add files via upload

---
+++ eduweb/tambah_produk.php
@@ -0,0 +1,78 @@
+<?php
+  include('koneksi.php'); //agar index terhubung dengan database, maka koneksi sebagai penghubung harus di include
+  
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> </title>
+    <style type="text/css">
+      * {
+        font-family: "Trebuchet MS";
+      }
+      h1 {
+        text-transform: uppercase;
+        color: salmon;
+      }
+    button {
+          background-color: salmon;
+          color: #fff;
+          padding: 10px;
+          text-decoration: none;
+          font-size: 12px;
+          border: 0px;
+          margin-top: 20px;
+    }
+    label {
+      margin-top: 10px;
+      float: left;
+      text-align: left;
+      width: 100%;
+    }
+    input {
+      padding: 6px;
+      width: 100%;
+      box-sizing: border-box;
+      background: #f8f8f8;
+      border: 2px solid #ccc;
+      outline-color: salmon;
+    }
+    div {
+      width: 100%;
+      height: auto;
+    }
+    .base {
+      width: 400px;
+      height: auto;
+      padding: 20px;
+      margin-left: auto;
+      margin-right: auto;
+      background: #ededed;
+    }
+    </style>
+  </head>
+  <body>
+      <center>
+        <h1>Tambah Program</h1>
+      <center>
+      <form method="POST" action="proses_tambah.php" enctype="multipart/form-data" >
+      <section class="base">
+        <div>
+          <label>Nama Program</label>
+          <input type="text" name="Nama Program" autofocus="" required="" />
+        </div>
+        <div>
+          <label>Harga</label>
+         <input type="text" name="Harga" required="" />
+        </div>
+        <div>
+          <label>Durasi</label>
+         <input type="text" name="Durasi" required="" />
+        </div>
+        <div>
+         <button type="submit">Simpan Program</button>
+        </div>
+        </section>
+      </form>
+  </body>
+</html>
